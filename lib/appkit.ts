2ac2f5f0b8c346e1f9be85fce92bbb3134f3a0d6
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-10-17 13:37:45 +0300

    Delete lib directory

--- lib/appkit.ts
+++
@@ -1,8 +0,0 @@
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { base } from 'viem/chains'
-
-export const wagmiAdapter = new WagmiAdapter({
-  chains: [base],
-})
-
-export const wagmiConfig = wagmiAdapter.wagmiConfig
