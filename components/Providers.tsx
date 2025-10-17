fce765798ad5714577c4feaa0a3533c58bb7e381
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-10-17 13:37:31 +0300

    Delete components directory

--- components/Providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import React from 'react'
-import { WagmiProvider } from 'wagmi'
-import { wagmiConfig } from '../lib/appkit'
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>
-}
