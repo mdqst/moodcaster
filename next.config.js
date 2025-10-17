f825644e02d2f047bba668b0984d2f91a4d752ba
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-10-17 12:49:49 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,2 +1,16 @@
-const nextConfig = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@react-native-async-storage/async-storage': false,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+};
+
 module.exports = nextConfig;
